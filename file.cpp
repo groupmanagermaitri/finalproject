22a12aa7275b47b5321ab521f0e5fe87c94d1751
maitri003 <kumarmaitri19@gmail.com>
2019-04-18 23:31:01 +0530

    fourth commit

--- file.cpp
+++ file.cpp
@@ -141,3 +141,70 @@ void main()
 	cout<<"\nPercentage = "<<perc<<"%";
 	getch();
 }
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int n, i, arr[50], sum=0;
+	cout<<"How many number you want to enter ?\n";
+	cin>>n;
+	cout<<"Enter "<<n<<" Numbers :";
+	for(i=0; i<n; i++)
+	{
+		cin>>arr[i];
+		sum=sum+arr[i];
+	}
+	int armean=sum/n;
+	cout<<"Arithmetic Mean = "<<armean;
+	getch();
+}
+#include<iostream.h>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+	clrscr();
+	float a, b, res;
+	char choice, ch;
+	do
+	{
+		cout<<"1.Addition\n";
+		cout<<"2.Subtraction\n";
+		cout<<"3.Multiplication\n";
+		cout<<"4.Division\n";
+		cout<<"5.Exit\n\n";
+		cout<<"Enter Your Choice : ";
+		cin>>choice;
+		switch(choice)
+		{
+			case '1' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a+b;
+				cout<<"Result = "<<res;
+				break;
+			case '2' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a-b;
+				cout<<"Result = "<<res;
+				break;
+			case '3' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a*b;
+				cout<<"Result = "<<res;
+				break;
+			case '4' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a/b;
+				cout<<"Result = "<<res;
+				break;
+			case '5' : exit(0);
+				break;
+			default : cout<<"Wrong Choice..!!";
+				break;
+		}
+		cout<<"\n------------------------------------\n";
+	}while(choice!=5 && choice!=getchar());
+	getch();
+}

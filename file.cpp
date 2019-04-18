0fbbcfc928b7da50b0c81c67870853bac05fcc01
maitri003 <kumarmaitri19@gmail.com>
2019-04-18 23:26:46 +0530

    fourth commit

--- file.cpp
+++ file.cpp
@@ -121,4 +121,23 @@ int main()
     return 0;
     return 0;
 }
-
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int mark[5], i;
+	float sum=0;
+	cout<<"Enter marks obtained in Physics, Chemistry, Maths, CS, English :";
+	for(i=0; i<5; i++)
+	{
+		cin>>mark[i];
+		sum=sum+mark[i];
+	}
+	float avg=sum/5;
+	float perc;
+	perc=(sum/500)*100;
+	cout<<"Average Marks = "<<avg;
+	cout<<"\nPercentage = "<<perc<<"%";
+	getch();
+}

7b1b30250a881b40a803c2cdc3e8e664dc42ed51
maitri003 <46710448+maitri003@users.noreply.github.com>
2019-04-22 20:05:22 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,90 +1,71 @@
-void Store(){
-     
-     cout << "The Item store is not finished yet\n" << endl;
-     
-     int Mainmenu();
-     Mainmenu();
-     
-     }
-     
-void Character(){
-     
-     int PlayerHp, PlayerStr, PlayerMana;
-     
-     cout << "Your character has:\n\n";
-               
-     cout << PlayerHp << " Health" << endl;
-     cout << PlayerStr << " Strength" << endl;
-     cout << PlayerMana << " Mana" << endl;
-     
-     cout << endl;
-     
-     int Mainmenu();
-     Mainmenu();
-     }
-     
-void Inventory(){
-     
-     cout << "Your inventory contains:\n" << endl;
-     
-     int Mainmenu();
-     Mainmenu();
-
-     }
-     
-void Wilderness(){
-     
-     cout << "You search the Jungle and find:\n" << endl;
-     
-     string Creatures[10];
-     
-     Creatures[0] = "1";
-     Creatures[1] = "2";
-     Creatures[2] = "3";
-     Creatures[3] = "4";
-     
-     cout << Creatures << " Is really dangerous " << endl;
-     
-     int Mainmenu();
-     Mainmenu();
-     }
-
-void Mainmenu(){
-     
-     string choice;
-     
-     cout << "1: Attack creature" << endl;
-     cout << "2: Buy equipment" << endl;
-     cout << "3: Inventory" << endl;
-     cout << "4: Stats" << endl;
-          
-          // invalid option returns to this
-          InvalidOption:
-                      
-     cin >> choice;
-     if (choice == "1"){
-                Wilderness ();
-                }
-     else if (choice == "2"){
-                     Store();
-                     }
-     else if (choice == "3"){
-                     Inventory();
-                     }
-     else if (choice == "4"){
-                     Character();
-                     }
-     else{
-          cout << "You have not chosen a valid option" << endl;
-          goto InvalidOption;
-     }
-                     
-
-     
-     }
-
-int main()
-{
-    int PlayerHp = 100, PlayerStr = 20, PlayerMana = 50;
-    Mainmenu ();
-}
+using namespace std; 
+  
+typedef long long LL; 
+typedef pair<int, int> pii; 
+typedef pair<LL, LL> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<LL> vl; 
+typedef vector<vl> vvl; 
+  
+double EPS = 1e-9; 
+int INF = 1000000005; 
+long long INFF = 1000000000000000005LL; 
+double PI = acos(-1); 
+int dirx[8] = { -1, 0, 0, 1, -1, -1, 1, 1 }; 
+int diry[8] = { 0, 1, -1, 0, -1, 1, -1, 1 }; 
+  
+#ifdef TESTING 
+#define DEBUG fprintf(stderr, "====TESTING====\n") 
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
+#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#else 
+#define DEBUG 
+#define VALUE(x) 
+#define debug(...) 
+#endif 
+  
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a)) 
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a)) 
+#define FORSQ(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a)) 
+#define FORC(a, b, c) for (char(a) = (b); (a) <= (c); ++(a)) 
+#define FOREACH(a, b) for (auto&(a) : (b)) 
+#define REP(i, n) FOR(i, 0, n) 
+#define REPN(i, n) FORN(i, 1, n) 
+#define MAX(a, b) a = max(a, b) 
+#define MIN(a, b) a = min(a, b) 
+#define SQR(x) ((LL)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a)) 
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
+#define ALL(v) v.begin(), v.end() 
+#define ALLA(arr, sz) arr, arr + sz 
+#define SIZE(v) (int)v.size() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
+#define PERMUTE next_permutation 
+#define TC(t) while (t--) 
+  
+inline string IntToString(LL a) 
+{ 
+    char x[100]; 
+    sprintf(x, "%lld", a); 
+    string s = x; 
+    return s; 
+} 
+  
+inline LL StringToInt(string a) 
+{ 
+    char x[100]; 
+    LL res; 
+    strcpy(x, a.c_str()); 
+    sscanf(x, "%lld", &res); 
+    return res; 
+} 
